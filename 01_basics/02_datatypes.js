f146d7a43c182e6473d1df4fbb288d90d86ffc7e
nandiniy788 <nandiniyadav917149@gmail.com>
2025-03-23 19:40:17 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use string "; // treat all js code as newer version
+// alert (3 + 3) //  i am using nodejs, not browser 
+console.log(3 + 3)// code readability is high 
+console.log("nandini")
+let name = "nandini"
+let age = 21
+let isLoggedIn = false 
+// number => 2 to power 53 
+// bigint 
+// boolean => true or false 
+// null => standalone value 
+// undefine =>
+// symbol => unique 
+// object 
+console.log(typeof false);
