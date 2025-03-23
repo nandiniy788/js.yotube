0668e50c654d1288a1d5a1b15348a76f51e25a90
nandiniy788 <nandiniyadav917149@gmail.com>
2025-03-23 09:57:23 +0000

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 144553 
+let accountEmail = "nandini@917149"
+var accountpassword = "12345"
+accountCity = "jaipure" 
+let accountstate;
+//accountId = 2 // not allow 
+accountEmail = "vaishu9892@gmai.com"
+accountpassword= "212122"
+accountCity = "Bengluru"
+console.log(accountId); 
+console.log([accountId, accountEmail,accountpassword,accountCity ,accountstate]);
+
