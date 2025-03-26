55f2c25ce8f5bb8efbf129a64acdb6a38f81f6e5
nandiniy788 <nandiniyadav917149@gmail.com>
2025-03-26 07:48:53 +0000

    memory

---
+++ 01_basics/memory.js
@@ -0,0 +1,20 @@
+// stact (primitive) ,Heap  (non-primitive)
+
+let myYoutubename  = hiteshchodharydotcom 
+let  anothername = myYoutubename
+anothename = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne 
+userTwo.com = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
